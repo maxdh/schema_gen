fdd78ebca760e6ebe6b18d000c2d99a4e295f5fc
Max Droog Hayes <max@mastodonc.com>
2014-07-24 16:41:55 +0100

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
-(defproject schema_gen "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject kixi/schema_gen "0.1.0"
+  :description "Data generation from Prismatic schemas"
+  :lein-release {:deploy-via :clojars}
+  :url "https://github.com/maxdh/schema_gen.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
 	         [prismatic/schema "0.2.4"]
-		 [org.clojure/test.check "0.5.8"]])
+		 [org.clojure/test.check "0.5.8"]]
+
+  :scm {:name "git"
+        :url "git@github.com:maxdh/schema_gen.git"})
