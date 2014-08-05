8d227bd5591642763e8bca724dbc3be460ac3855
Max Droog Hayes <max@mastodonc.com>
2014-08-05 16:59:50 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-	         [prismatic/schema "0.2.4"]
-		 [org.clojure/test.check "0.5.8"]
+	         [prismatic/schema "0.2.6"]
+		 [org.clojure/test.check "0.5.9"]
                  [schema-contrib "0.1.3"]
                  [clj-time "0.7.0"]]
 
