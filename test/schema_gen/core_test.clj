72376e7d14d61b3e6cd8001295b58ec99b8aa034
Max Droog Hayes <max@mastodonc.com>
2014-08-11 14:05:55 +0100

    A couple of tests as examples.

--- test/schema_gen/core_test.clj
+++ test/schema_gen/core_test.clj
@@ -1,7 +1,25 @@
 (ns schema-gen.core-test
   (:require [clojure.test :refer :all]
-            [schema-gen.core :refer :all]))
+            [schema_gen.core :refer :all]
+            [schema.core :as s]
+            [schema-contrib.core :as sc]
+            ))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def ^:private test-map-schema
+  {:string s/Str
+   :int s/Int
+   :nestedmap {:num s/Num
+               :key s/Keyword}
+   :date sc/ISO-Date-Time})
+
+(deftest map-test
+  (is (true? (validator test-map-schema))))
+
+(def ^:private test-vector-schema
+  [(s/one s/Str "Str")
+   (s/one s/Int "Int")
+   (s/one s/Num "Num")
+   sc/ISO-Date-Time])
+
+(deftest vector-test
+  (is (true? (validator test-vector-schema))))
